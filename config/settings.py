6f2a3a378f35c16b4b5220027f2fe437d10864a6
Djunpy <djunpy@gmail.com>
2022-04-17 01:22:39 +0300

    added

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = decouple.config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
