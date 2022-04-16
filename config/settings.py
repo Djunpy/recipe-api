9e2337b614e03ac156438cd96a2c3faa70e3613b
Djunpy <djunpy@gmail.com>
2022-04-16 23:52:25 +0300

    deploy2

--- config/settings.py
+++ config/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'conig.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'conig.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
